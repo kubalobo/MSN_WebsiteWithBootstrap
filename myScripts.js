b68f631c49f1965041a9775711e5d9a305feff56
Łobodecki Jakub <jakub.lobodecki@pw.edu.pl>
2020-11-18 09:08:23 +0100

    Basic leaflet added

--- myScripts.js
+++ myScripts.js
@@ -4,3 +4,21 @@ $('#myButton').click(
         console.log("Hello! 🔥")
     }
 )
+
+
+
+
+var mymap = L.map('mapid').setView([19.708095, -155.595600], 7);
+
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri'
+}).addTo(mymap);
+
+var markersContainer = [];
+
+function onMapClick(event) {
+    markersContainer.push(L.marker(event.latlng).addTo(mymap));
+    console.log(markersContainer);
+}
+
+mymap.on('click', onMapClick)
